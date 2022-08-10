7a2517c69952b84175dafacc1e4ded59bef7cf6c
dennisg2000 <84654444+dennisg2000@users.noreply.github.com>
2022-08-09 21:07:53 -0500

    Update Persona.java

--- src/main/java/com/example/project/Persona.java
+++ src/main/java/com/example/project/Persona.java
@@ -7,4 +7,19 @@ public class Persona {
         this.DNI = DNI;
         this.nombre = nombre;
     }
+    public String getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
